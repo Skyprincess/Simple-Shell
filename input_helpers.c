3039b86fa633abb118373f08d0018f0395880c42
Skyprincess <iyaseamaka@gmail.com>
2022-12-21 10:54:39 -0800

    input helpers

--- input_helpers.c
+++ input_helpers.c
@@ -11,26 +11,22 @@ int check_args(char **args);
  * @line: A buffer to store the command.
  * @exe_ret: The return value of the last executed command.
  * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the stored command.
+ *	   Otherwise - a pointer to the stored command.
  */
 char *get_args(char *line, int *exe_ret)
 {
 	size_t n = 0;
 	ssize_t read;
 	char *prompt = "$ ";
-int hist;
 
 	if (line)
 		free(line);
 	read = _getline(&line, &n, STDIN_FILENO);
-
 	if (read == -1)
 		return (NULL);
-
 	if (read == 1)
 	{
 		hist++;
-
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, prompt, 2);
 		return (get_args(line, exe_ret));
@@ -56,15 +52,32 @@ int call_args(char **args, char **front, int *exe_ret)
 		return (*exe_ret);
 	for (index = 0; args[index]; index++)
 	{
-
 		if (_strncmp(args[index], "||", 2) == 0)
 		{
 			free(args[index]);
 			args[index] = NULL;
 			args = replace_aliases(args);
 			ret = run_args(args, front, exe_ret);
-
 			if (*exe_ret != 0)
+			{
+				args = &args[++index];
+				index = 0;
+			}
+			else
+			{
+
+				for (index++; args[index]; index++)
+					free(args[index]);
+				return (ret);
+			}
+		}
+		else if (_strncmp(args[index], "&&", 2) == 0)
+		{
+			free(args[index]);
+			args[index] = NULL;
+			args = replace_aliases(args);
+			ret = run_args(args, front, exe_ret);
+			if (*exe_ret == 0)
 			{
 				args = &args[++index];
 				index = 0;
@@ -75,30 +88,11 @@ int call_args(char **args, char **front, int *exe_ret)
 					free(args[index]);
 				return (ret);
 			}
-}
-else if (_strncmp(args[index], "&&", 2) == 0)
-			{
-				free(args[index]);
-				args[index] = NULL;
-				args = replace_aliases(args);
-				ret = run_args(args, front, exe_ret);
-
-				if (*exe_ret == 0)
-				{
-					args = &args[++index];
-					index = 0;
-				}
-				else
-				{
-					for (index++; args[index]; index++)
-						free(args[index]);
-					return (ret);
-				}
-			}
 		}
-		args = replace_aliases(args);
-		ret = run_args(args, front, exe_ret);
-		return (ret);
+	}
+	args = replace_aliases(args);
+	ret = run_args(args, front, exe_ret);
+	return (ret);
 }
 
 /**
@@ -112,14 +106,11 @@ int run_args(char **args, char **front, int *exe_ret)
 {
 	int ret, i;
 	int (*builtin)(char **args, char **front);
-int hist;
 
 	builtin = get_builtin(args[0]);
-
 	if (builtin)
 	{
 		ret = builtin(args + 1, front);
-
 		if (ret != EXIT)
 			*exe_ret = ret;
 	}
@@ -147,12 +138,10 @@ int handle_args(int *exe_ret)
 	char **args, *line = NULL, **front;
 
 	line = get_args(line, exe_ret);
-
 	if (!line)
 		return (END_OF_FILE);
 	args = _strtok(line, " ");
 	free(line);
-
 	if (!args)
 		return (ret);
 	if (check_args(args) != 0)
@@ -162,7 +151,6 @@ int handle_args(int *exe_ret)
 		return (*exe_ret);
 	}
 	front = args;
-
 	for (index = 0; args[index]; index++)
 	{
 		if (_strncmp(args[index], ";", 1) == 0)
@@ -194,14 +182,11 @@ int check_args(char **args)
 	for (i = 0; args[i]; i++)
 	{
 		cur = args[i];
-
 		if (cur[0] == ';' || cur[0] == '&' || cur[0] == '|')
 		{
-
 			if (i == 0 || cur[1] == ';')
 				return (create_error(&args[i], 2));
 			nex = args[i + 1];
-
 			if (nex && (nex[0] == ';' || nex[0] == '&' || nex[0] == '|'))
 				return (create_error(&args[i + 1], 2));
 		}
